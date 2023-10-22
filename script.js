a318dd0ab9b09774916875565b4ecaf81d2fec3a
theprincesbride <140436808+theprincesbride@users.noreply.github.com>
2023-10-21 20:34:07 -0400

    created variables num1, num2 and operator.  Created operate function which will call the above variables and perform the previously defined functions.  Complted steps two & three on TOP's project page.

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+let num1;
+let num2;
+let operator;
+
+function operate(operator, num1, num2) {
+    let answer;
+    if (operator === '+') {
+        answer = add(num1, num2);
+    }
+    else if (operator === '-') {
+        answer = subtract(num1, num2);
+    }
+    else if (operator === 'x' || operator === '*') {
+        answer = multiply(num1, num2);
+    }
+    else if (operator === '/' ) {
+        answer = divide(num1, num2);
+    } return answer;
+}
